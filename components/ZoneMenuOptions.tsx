9417f31d8a4f0cf1d574ec2fc3a28fe3fd3b5b6d
Erik Sundberg <erik.sundberg@unicus.com>
2023-06-18 11:56:17 +0200

    separate animation methods

--- components/ZoneMenuOptions.tsx
+++ components/ZoneMenuOptions.tsx
@@ -1,52 +1,21 @@
-import { Ionicons } from "@expo/vector-icons";
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { useRef, useLayoutEffect } from "react";
-import { Animated, Pressable } from "react-native";
-import { RootStackParamList } from "../navigators/AppNavigator";
-import { AppColors } from "../theme/colors";
+import { Ionicons } from '@expo/vector-icons';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { useRef, useLayoutEffect, createRef } from 'react';
+import { Animated, Pressable } from 'react-native';
+import { RootStackParamList } from '../navigators/AppNavigator';
+import { AppColors } from '../theme/colors';
 
 //TODO: move to separate file
 export const MenuOptions = ({
   navigation,
 }: {
-  navigation: NativeStackNavigationProp<RootStackParamList, 'Zone'>;
+  navigation: NativeStackNavigationProp<RootStackParamList, 'ZoneHome'>;
 }) => {
   const fadeAnim = useRef(new Animated.Value(0)).current;
   const translateAnim = useRef(new Animated.Value(20)).current;
+  const initialValues = { fadeAnim, translateAnim };
 
-  const translateIn = () => {
-    Animated.timing(translateAnim, {
-      toValue: 0,
-      duration: 200,
-      useNativeDriver: true,
-    }).start();
-  };
-
-  const fadeIn = () => {
-    // Will change fadeAnim value to 1 in 5 seconds
-    Animated.timing(fadeAnim, {
-      toValue: 1,
-      duration: 200,
-      useNativeDriver: true,
-    }).start();
-  };
-
-  const fadeOut = () => {
-    // Will change fadeAnim value to 0 in 3 seconds
-    Animated.timing(fadeAnim, {
-      toValue: 0,
-      duration: 300,
-      useNativeDriver: true,
-    }).start();
-  };
-
-  useLayoutEffect(() => {
-    translateIn();
-    fadeIn();
-    return () => {
-      fadeOut();
-    };
-  }, []);
+  useLayoutEffect(runMenuAnimation(initialValues), []);
 
   return (
     <Animated.View
@@ -81,6 +50,17 @@ export const MenuOptions = ({
           borderRadius: 5,
           margin: 10,
         }}
+        onPress={() => navigation.navigate('Zone Chat')}
+      >
+        <Ionicons name={'chatbox-outline'} size={30} color={'white'} />
+      </Pressable>
+      <Pressable
+        style={{
+          padding: 10,
+          backgroundColor: AppColors.success700,
+          borderRadius: 5,
+          margin: 10,
+        }}
         onPress={() => navigation.navigate('Home', { deviceId: '123' })}
       >
         <Ionicons name={'md-exit-outline'} size={30} color={'white'} />
@@ -88,3 +68,46 @@ export const MenuOptions = ({
     </Animated.View>
   );
 };
+
+const translateIn = (translateAnim: Animated.Value) => {
+  Animated.timing(translateAnim, {
+    toValue: 0,
+    duration: 200,
+    useNativeDriver: true,
+  }).start();
+};
+
+const fadeIn = (fadeAnim: Animated.Value) => {
+  // Will change fadeAnim value to 1 in 5 seconds
+  Animated.timing(fadeAnim, {
+    toValue: 1,
+    duration: 200,
+    useNativeDriver: true,
+  }).start();
+};
+
+const fadeOut = (fadeAnim: Animated.Value) => {
+  // Will change fadeAnim value to 0 in 3 seconds
+  Animated.timing(fadeAnim, {
+    toValue: 0,
+    duration: 300,
+    useNativeDriver: true,
+  }).start();
+};
+
+//TODO: move to separate file/utils?
+const runMenuAnimation =
+  ({
+    translateAnim,
+    fadeAnim,
+  }: {
+    translateAnim: Animated.Value;
+    fadeAnim: Animated.Value;
+  }) =>
+  () => {
+    translateIn(translateAnim);
+    fadeIn(fadeAnim);
+    return () => {
+      fadeOut(fadeAnim);
+    };
+  };
